7f1e65ca82c396aecc49084e1b63d8d7b48a2fa8
ssuperlg <ssuperlglg@gmail.com>
2018-11-26 17:48:51 +0800

    learn case

--- haskell/chap3/chapter3.hs
+++ haskell/chap3/chapter3.hs
@@ -13,3 +13,40 @@ pluralise word counts = map plural counts
         where plural 0 = "no" ++ word ++"s"
               plural 1 = "one" ++ word
               plural n = show n ++ " " ++ word ++ "s"
+
+ 
+bar = let a = 1
+          b = 2
+          c = 3
+      in a + b + c
+
+foo2 = let { a = 1;b = 2;
+            c = 3 }
+            in a + b + c
+
+data Fruit = Apple | Orange
+            deriving(Show)
+
+betterFruit f = case f of
+                  "apple"  -> Apple
+                  "orange" -> Orange
+      
+lend3 amount balance
+      | amount <= 0           = Nothing
+      | amount > reserve * 0.5= Nothing
+      | otherwise             = Just newBalance
+      where reserve = 100
+            newBalance = balance - amount
+
+-- count Number List length
+countLength (x:xs) = 1 + countLength(xs)
+countLength [] = 0
+
+countList (x:xs) = x + countList(xs)
+countList [] = 0
+
+avgList (x:xs) = countList(x:xs) / countLength(x:xs)
+avgList [] = 0
+
+-- 回文 123 -> 123321
+descList (x:xs) = [x]
