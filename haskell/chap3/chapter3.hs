7d7d943e8d45e19ed5184b61ccd190d0c82d9b9a
wangpenghui <wangpenghui@iboxpay.com>
2018-11-26 09:22:41 +0800

    test

--- haskell/chap3/chapter3.hs
+++ haskell/chap3/chapter3.hs
@@ -12,4 +12,4 @@ pluralise :: String -> [Int] -> [String]
 pluralise word counts = map plural counts
         where plural 0 = "no" ++ word ++"s"
               plural 1 = "one" ++ word
-          plural n = show n ++ " " ++ word ++ "s"
+              plural n = show n ++ " " ++ word ++ "s"
